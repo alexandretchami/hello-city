2e7b034294ecc8a9765841aa1e1c8816feef5203
BlackLivesMatter <tchamistephane@yahoo.fr>
2021-05-21 14:26:23 +0200

    Add twig and maker bundle, Add php.ini config, Setup PagesController and homepage

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -13,6 +13,17 @@ class PagesController extends AbstractController
      */
     public function home(): Response
     {
+
         return $this->render('pages/home.html.twig');
     }
-}
+    
+      /**
+     * @Route("/about-us", name="app_about")
+     */
+    public function about(): Response
+    {
+
+        return $this->render('pages/about.html.twig');
+    }
+} 
+
